f86d8784cbdbb3af4ad0fcb745a3859d89d91fa0
burntato <kineticz300@gmail.com>
2022-02-18 20:49:45 +0700

    fix npm run watch

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+
+module.exports = {
+
+    plugins: [
+        new WebpackShellPluginNext({
+            onBuildStart: {
+                scripts: ['echo "Webpack Start"'],
+                blocking: true,
+                parallel: false
+            },
+            onBuildEnd: {
+                scripts: ['echo "Webpack End"'],
+                blocking: false,
+                parallel: true
+            }
+        })
+    ],
+
+}
